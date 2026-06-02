f36cac239d87e244f46c7c42f32ed54b89a9fe2c
kitsu <kitsuneofficialval@gmail.com>
2026-06-02 19:45:16 +0800

    x

--- Assets/Scripts/Player/HandGrabHandler.cs
+++ Assets/Scripts/Player/HandGrabHandler.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+enum GrabState
+{
+    None, // grab not pressed
+    Searching, // grab is pressed - searching for grab target
+    Reaching, // grab target found, hands reaching
+    Attached // hands attached to the target
+}
+
 public class HandGrabHandler : MonoBehaviour
 {
     
